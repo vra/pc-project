f7f95446ad166abbb79f5d3062980423900f4ab8
Yunfeng Wang <wangyf11@mail.ustc.edu.cn>
2016-11-28 23:01:43 +0800

    add ingore file,format cuda code

--- projects/1/cuda/num_prime.cu
+++ projects/1/cuda/num_prime.cu
@@ -4,14 +4,17 @@
 #include <iostream>
 #include <sys/time.h>
 
-//code do main the job on GPU
+//code that does the main job on GPU
 __global__ void countNumOfPrimerKernel(int* n_array_d, bool* is_prime_d)
 {
 	int col = blockIdx.x * blockDim.x + threadIdx.x;
 	int row = blockIdx.y * blockDim.y + threadIdx.y;
 	int i = row * blockDim.x + col + 2;
 	
-//	printf("gridDim: %d,block.x: %d, block.y: %d, thread.x: %d, thread:y: %d, col: %d, row: %d, i: %d\n",gridDim.x, blockIdx.x, blockIdx.y, threadIdx.x, threadIdx.y, col, row, i);
+//	printf("gridDim: %d,block.x: %d, block.y: %d, thread.x: %d, thread:y: %d,\
+// col: %d, row: %d, i: %d\n",gridDim.x, blockIdx.x, blockIdx.y, threadIdx.x,\
+// threadIdx.y, col, row, i);
+
 	bool has_factor = false;
 	for (int j = 2; j < i; ++j)
 	{
@@ -34,7 +37,8 @@ int main(int argc, char** argv)
 	gettimeofday(&start, NULL);
 	if (argc != 3)
 	{
-		std::cout << "USAGE: num_prime <number of blocks> <integer>" << std::endl;
+		std::cout << "USAGE: num_prime <number of blocks> <integer>" 
+				  << std::endl;
 		return -1;
 	}
 	
@@ -60,13 +64,16 @@ int main(int argc, char** argv)
 	cudaMemcpy(is_prime_d, is_prime, n * sizeof(bool), cudaMemcpyHostToDevice);
 	
 	num_blocks = 1000 ;
-	int num_thread = n / num_blocks; // use fixed number of thread in each block for convience
-	printf("num_blocks:%d, num_thread:%d\n", num_blocks, num_thread);
+
+	// use fixed number of thread in each block for convience
+	int num_thread = n / num_blocks; 
+	printf("num_blocks: %d, num_thread: %d\n", num_blocks, num_thread);
 	dim3 dimBlock(num_thread, num_thread);
 	dim3 dimGrid(num_blocks, 1);
 
 	//countNumOfPrimerKernel<<<dimGrid, dimBlock>>> (n_array_d, is_prime_d);
-	countNumOfPrimerKernel<<< dim3(num_blocks), dim3(num_thread)>>> (n_array_d, is_prime_d);
+	countNumOfPrimerKernel<<< dim3(num_blocks), dim3(num_thread)>>> \
+		(n_array_d, is_prime_d);
 	cudaThreadSynchronize();
 
 	cudaMemcpy(is_prime, is_prime_d, n * sizeof(bool), cudaMemcpyDeviceToHost);
@@ -81,8 +88,8 @@ int main(int argc, char** argv)
 		}
 	}
 	
-	std::cout << "Number of primes between 0 and " << n << " is: " << sum_num_prime << std::endl;
-	
+	std::cout << "Number of primes between 0 and " << n << " is: " 
+			  << sum_num_prime << std::endl;
 	
 	
 	cudaFree(n_array_d);
@@ -92,7 +99,8 @@ int main(int argc, char** argv)
 	delete[] is_prime; 
 
 	gettimeofday(&end, NULL);
-	double time_gap = (end.tv_sec - start.tv_sec) * 1000000u + end.tv_usec - start.tv_usec;	
+	double time_gap = (end.tv_sec - start.tv_sec) * 1000000u 
+					+ end.tv_usec - start.tv_usec;	
 	printf("Time cost: %.2lf s.\n", time_gap / 100000);       
 
 	return 0;
